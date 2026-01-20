0e2b94338d737be73565601c4245b76e95e9d7fe
Carson <cjplett@gmail.com>
2026-01-20 15:17:39 -0500

    Initial implementation of mySqrt().

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mySqrt(int x){
+    if(x==0 || x==1) return x;
+    int i = 1;
+    int temp = 0;
+    while(i*i<=x){
+        temp = i++;
+        if(i==46341) return temp;
+    }
+    return temp;
+}
+
+int main(int argc, char *argv[]){
+    int s = mySqrt(57);
+    printf("The integer square root is: %d", s);
+    return 0;
+}
